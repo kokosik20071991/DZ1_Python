30f743f7810612fd5e76125192f155d34cbc951a
Viktoria <vikatim4encko@yandex.ru>
2023-02-28 15:36:16 +0300

    Задача №8

---
+++ DZ_1/ZD_8.py
@@ -0,0 +1,7 @@
+n = int(input('Введите n: '))
+m = int(input('Введите m: '))
+k = int(input('Введите k: '))
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
